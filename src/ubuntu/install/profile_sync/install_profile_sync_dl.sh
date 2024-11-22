4894abb8df98f599adde803c632bf7cdd03d82d5
shucheng <shucheng@bd-apaas.com>
2024-11-23 01:56:53 +0800

    install_profile_sync_dl.sh

--- src/ubuntu/install/profile_sync/install_profile_sync_dl.sh
+++ src/ubuntu/install/profile_sync/install_profile_sync_dl.sh
@@ -18,7 +18,25 @@ COMMIT_ID="42424ea385a0d10fa7bb5749e207ee70b2a44ae2"
 BRANCH="develop"
 COMMIT_ID_SHORT=$(echo "${COMMIT_ID}" | cut -c1-6)
 
-profile_distro=$(detect_deb_distro)
+profile_distro="debian_bookworm"
+
+echo $profile_distro
+
+ARCH=x86_64
+BINARY_NAME="${profile_distro}_${BRANCH}_${COMMIT_ID_SHORT}_${ARCH}-kasm-profile-sync"
+BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/profile-sync/${COMMIT_ID}/${BINARY_NAME}"
+curl -x $SOCKS5_PROXY_LOCAL \
+  -o .tmp/profile_sync/${BINARY_NAME} \
+  -fL ${BUILD_URL}
+
+ARCH=aarch64
+BINARY_NAME="${profile_distro}_${BRANCH}_${COMMIT_ID_SHORT}_${ARCH}-kasm-profile-sync"
+BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/profile-sync/${COMMIT_ID}/${BINARY_NAME}"
+curl -x $SOCKS5_PROXY_LOCAL \
+  -o .tmp/profile_sync/${BINARY_NAME} \
+  -fL ${BUILD_URL}
+
+profile_distro="ubuntu_jammy"
 
 echo $profile_distro
 
