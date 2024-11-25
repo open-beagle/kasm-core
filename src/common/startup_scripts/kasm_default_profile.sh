369f9bfe40d5de3db48a47b9214653981fe73c5d
shucheng <shucheng@bd-apaas.com>
2024-11-26 01:10:40 +0800

    /run/dbus /var/log/beagle

--- src/common/startup_scripts/kasm_default_profile.sh
+++ src/common/startup_scripts/kasm_default_profile.sh
@@ -3,57 +3,54 @@ set -ex
 DEFAULT_PROFILE_HOME=/home/kasm-default-profile
 PROFILE_SYNC_DIR=/kasm_profile_sync
 
-
 function copy_default_profile_to_home {
-    echo "Copying default profile to home directory"
-    cp -r $DEFAULT_PROFILE_HOME/*  $HOME/
-    ls -la $HOME
+  echo "Copying default profile to home directory"
+  cp -r $DEFAULT_PROFILE_HOME/* $HOME/
+  ls -la $HOME
 }
 
 function verify_profile_config {
-    echo "Verifying Uploads/Downloads Configurations"
+  echo "Verifying Uploads/Downloads Configurations"
 
-    mkdir -p $HOME/Uploads
+  mkdir -p $HOME/Uploads
 
-    if [ -d "$HOME/Desktop/Uploads" ]; then
-        echo "Uploads Desktop Symlink Exists"
-    else
-        echo "Creating Uploads Desktop Symlink"
-        ln -sf $HOME/Uploads $HOME/Desktop/Uploads
-    fi
+  if [ -d "$HOME/Desktop/Uploads" ]; then
+    echo "Uploads Desktop Symlink Exists"
+  else
+    echo "Creating Uploads Desktop Symlink"
+    ln -sf $HOME/Uploads $HOME/Desktop/Uploads
+  fi
 
+  mkdir -p $HOME/Downloads
 
-    mkdir -p $HOME/Downloads
+  if [ -d "$HOME/Desktop/Downloads" ]; then
+    echo "Downloads Desktop Symlink Exists"
+  else
+    echo "Creating Download Desktop Symlink"
+    ln -sf $HOME/Downloads $HOME/Desktop/Downloads
+  fi
 
-    if [ -d "$HOME/Desktop/Downloads" ]; then
-        echo "Downloads Desktop Symlink Exists"
-    else
-        echo "Creating Download Desktop Symlink"
-        ln -sf $HOME/Downloads $HOME/Desktop/Downloads
-    fi
+  if [[ "$(readlink -f $KASM_VNC_PATH/www/Downloads/Downloads)" != "$HOME/Downloads" ]]; then
+    echo "Fixing Downloads RX Symlink"
+    rm -f $KASM_VNC_PATH/www/Downloads/Downloads
+    ln -sf $HOME/Downloads $KASM_VNC_PATH/www/Downloads/Downloads
+  else
+    echo "Downloads RX Symlink Exists"
+  fi
 
-
-    if [[ "$(readlink -f $KASM_VNC_PATH/www/Downloads/Downloads)" != "$HOME/Downloads" ]]; then
-        echo "Fixing Downloads RX Symlink"
-        rm -f $KASM_VNC_PATH/www/Downloads/Downloads
-        ln -sf $HOME/Downloads $KASM_VNC_PATH/www/Downloads/Downloads
-    else
-        echo "Downloads RX Symlink Exists"
-    fi
-
-
-    ls -la $HOME/Desktop
+  ls -la $HOME/Desktop
 
 }
 
-if  [ -f "$HOME/.bashrc" ]; then
-    echo "Profile already exists. Will not copy default contents"
+if [ -f "$HOME/.bashrc" ]; then
+  echo "Profile already exists. Will not copy default contents"
 else
-    echo "Profile Sync Directory Does Not Exist. No Sync will occur"
-    copy_default_profile_to_home
+  echo "Profile Sync Directory Does Not Exist. No Sync will occur"
+  copy_default_profile_to_home
 fi
 
 function start_supervisord {
+  sudo mkdir -p /run/dbus /var/log/beagle
   sudo chmod +x /usr/bin/dbus
   sudo /usr/bin/supervisord -c /etc/beagle/supervisord/supervisord.conf &
 }
